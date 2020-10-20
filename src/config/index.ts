55e87e63c05e7bb4f3cab81a8b3621024a5290cb
niruhsa <>
2020-10-20 20:18:37 +1100

    updates

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,14 @@
+import Database from "../database";
+
 export default class Config {
+
+    private db: Database = new Database();
 
     constructor() { this.loadConfig(); }
 
     private loadConfig() { }
 
     public getDeletedMessagesChannel() {
-        
+        const config = this.db.getConfig();
     }
 }
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
 export default class Database {
     
+
+    public getConfig() {
+        return new Promise(async (resolve: any) => {
+            const sql = [ `SELECT * FROM config`, [] ];
+        });
+    }
+
+    public getTrackedYouTubeChannels() {
+        return new Promise(async (resolve: any) => {
+            const sql = [ `SELECT * FROM yt_channels`, [] ]
+        });
+    }
+
 }
